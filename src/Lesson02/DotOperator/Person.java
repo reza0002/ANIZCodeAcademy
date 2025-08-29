c673bf8f8dbcc36e265740af251161a354074126
AnikoZs <azsigri@gmail.com>
2025-08-29 08:14:59 +0200

    lesson02

---
+++ src/Lesson02/DotOperator/Person.java
@@ -0,0 +1,17 @@
+package Lesson02.DotOperator;
+
+public class Person {
+    // Attributter
+    String firstName;
+    String lastName;
+    String middleName;
+    int age;
+
+    public void WritePersonInfo()
+    {
+        // Udskriver oplysninger
+        System.out.println("Navn: " + firstName + " " + middleName + " " + lastName);
+        System.out.println("Alder: " + age);
+    }
+}
+
